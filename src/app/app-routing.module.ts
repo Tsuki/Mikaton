d6b318a83abc7a729eee65739efa822719898336
Tsukatsuki Kanade <i@sukitsuki.com>
2018-06-12 19:54:18 +0700

    add layout scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HeaderAsideFooterLayoutComponent} from './layout/header-aside-footer-layout/header-aside-footer-layout.component';
+import {HeaderAsideFooterResponsiveLayoutComponent} from './layout/header-aside-footer-responsive-layout/header-aside-footer-responsive-layout.component';
+import {HeaderFooterLayoutComponent} from './layout/header-footer-layout/header-footer-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header-aside-footer-layout', component: HeaderAsideFooterLayoutComponent},
+  {path: 'header-aside-footer-responsive-layout', component: HeaderAsideFooterResponsiveLayoutComponent},
+  {path: 'header-footer-layout', component: HeaderFooterLayoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
