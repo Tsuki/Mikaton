15ba2cc414f8df520852951ca116bf199ce6c211
Tsukatsuki Kanade <i@sukitsuki.com>
2018-06-14 19:43:22 +0700

    Add index routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  display = false;
+
+  constructor(private router: Router, private location: Location) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((next) => {
+      if (next instanceof NavigationEnd) {
+        this.display = next.url === '/';
+      }
+    });
+  }
 }
